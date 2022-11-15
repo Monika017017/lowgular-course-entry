bdfc07b85e088fda8a8670266d1a1b66b5d101be
Monika017017 <m.piatek017@gmail.com>
2022-11-15 22:20:17 +0100

    feat: added font color to title and list

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -9,8 +9,13 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
-    name: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required]),
     age: new FormControl(null, [Validators.min(1), Validators.required]),
     salary: new FormControl(null, [Validators.min(1), Validators.required])
   });
+
+  onButtonClicked(form: {email: string, age: string, salary: string}) {
+    alert('User was succesfully added to the database. ' + 'Email: '+ form.email  + 'Age: ' + form.age + 'Salary: ' + form.salary);
+  }
+
 }
