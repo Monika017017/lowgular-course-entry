67007503154551b40e2333103a4c498648747e14
Monika017017 <m.piatek017@gmail.com>
2022-11-16 18:08:27 +0100

    feat: added font color to title and list

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
-import {PersonModel} from "../model/person.model";
-import {CreateEmployeeModel} from "../model/create-employee.model";
+import { Observable } from 'rxjs';
+import { PersonModel } from '../model/person.model';
+import { CreateEmployeeModel } from '../model/create-employee.model';
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
   getAll(): Observable<PersonModel[]> {
-    return this ._httpClient.get<PersonModel[]>('assets/data/people.json');
+    return this._httpClient.get<PersonModel[]>('assets/data/people.json');
   }
-  create(employee: CreateEmployeeModel): Observable<any> {
-    return this._httpClient.post('https://dummy.restapiexample.com/create', employee);
+  create(employee: CreateEmployeeModel): Observable<void> {
 
   }
 }
